bc92bc651168ed818048083a6b9721183d878efe
Krzysztof W <45880989+iProXDPL@users.noreply.github.com>
2023-02-12 23:26:24 +0100

    elo

--- Api/index.js
+++ Api/index.js
@@ -13,16 +13,14 @@ client.connect();
 app.use(cors());
 app.use(express.json());
 
-app.post("/walki", async (req, res) => {
-  if (req.body.itemy.length !== 0) {
-    try {
-      const database = client.db("walki");
-      const test = database.collection("vsMob");
-      var result = await test.insertOne(req.body);
-      console.log(`Dodano walkę z itemem ${result.insertedId}`);
-    } catch (error) {
-      console.log(error);
-    }
+app.post("/addvsMob", async (req, res) => {
+  try {
+    const database = client.db("walki");
+    const test = database.collection("vsMob");
+    var result = await test.insertOne(req.body);
+    console.log(`Dodano walkę z itemem ${result.insertedId}`);
+  } catch (error) {
+    console.log(error);
   }
   res.sendStatus(200);
 });
@@ -32,6 +30,62 @@ app.get("/vsMob", async (req, res) => {
     const database = client.db("walki");
     const test = await database.collection("vsMob").find({}).toArray();
     res.json(test);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/updatevsMob", async (req, res) => {
+  try {
+    res.sendStatus(200);
+    if (req.body.ev !== undefined) {
+      const database = client.db("walki");
+      const test = await database
+        .collection("vsMob")
+        .updateOne(
+          { ev: parseFloat(req.body.ev) },
+          { $set: { kto: req.body.kto } }
+        );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/addvsPlayer", async (req, res) => {
+  try {
+    const database = client.db("walki");
+    const test = database.collection("vsPlayer");
+    var result = await test.insertOne(req.body);
+    console.log(`Walkę z graczem ${result.insertedId}`);
+  } catch (error) {
+    console.log(error);
+  }
+  res.sendStatus(200);
+});
+
+app.get("/vsPlayer", async (req, res) => {
+  try {
+    const database = client.db("walki");
+    const test = await database.collection("vsPlayer").find({}).toArray();
+    res.json(test);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/updatevsPlayer", async (req, res) => {
+  try {
+    res.sendStatus(200);
+    if (req.body.ev !== undefined) {
+      const database = client.db("walki");
+      const test = await database
+        .collection("vsPlayer")
+        .updateOne(
+          { ev: parseFloat(req.body.ev) },
+          { $set: { log: req.body.log } }
+        );
+    }
   } catch (error) {
     console.log(error);
   }
--- tampler.js
+++ tampler.js
@@ -1,44 +1,63 @@
-function zapiszloot() {
-  var itemy = [];
+var send = {};
+var mlog = [];
+function oneClick(allData) {
+  //One click
+  First(allData);
+  Last(allData);
+}
+function First(allData) {
+  //First
+  send = {};
+  var war = Object.values(allData.f.w);
   var team = [];
   var enemy = [];
-  if (Engine.battle.myteam == 1) {
-    team = Engine.battle.getFlist1();
-    enemy = Engine.battle.getFlist2();
-  } else {
-    enemy = Engine.battle.getFlist1();
-    team = Engine.battle.getFlist2();
+  war.forEach((play) => {
+    if (allData.f.myteam == play.team) {
+      team.push(play);
+    } else {
+      enemy.push(play);
+    }
+  });
+  Object.assign(send, { team: team });
+  Object.assign(send, { enemy: enemy });
+}
+function Last(allData) {
+  if (allData.hasOwnProperty("loot")) {
+    Object.assign(send, { loot: Object.values(allData.item) });
   }
-  var loot = Engine.items.fetchLocationItems("l");
-  loot.forEach((element) => {
-    itemy.push({
-      id: element.id,
-      hid: element.hid,
-      tpl: element.tpl,
-      name: element.name,
-      icon: element.icon,
-      cl: element.cl,
-      pr: element.pr,
-      prc: element.prc,
-      stat: element.stat,
-    });
-  });
-  var data = JSON.stringify({
-    itemy: itemy,
-    team: team,
-    enemy: enemy,
-    ev: Engine.ev,
-    date: new Date().toJSON(),
-  });
-  $.ajax({
-    type: "POST",
-    url: "http://127.0.0.1:8080/walki",
-    data: data,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    dataType: "json",
-  });
+  Object.assign(send, { ev: allData.ev });
+  Object.assign(send, { data: new Date().toJSON() });
+  if (
+    send.loot !== undefined &&
+    send.loot.length !== 0 &&
+    send.enemy[0].id < 0
+  ) {
+    Object.assign(send, { kto: [] });
+    if (send.team.length == 1) {
+      var k = [];
+      send.loot.forEach((item) => {
+        k.push(Engine.hero.d.nick);
+        k.push("ITEM#" + item.hid);
+        k.push(item.name);
+      });
+      Object.assign(send, { kto: k });
+    }
+  }
+  if (send.hasOwnProperty("team")) {
+    if (send.enemy[0].id < 0) {
+      //Mob
+      sendvsMob(send);
+      mlog = [];
+    } else {
+      Object.assign(send, { log: mlog });
+      sendvsPlayer(send);
+      mlog = [];
+    }
+  }
+}
+
+function Dead(allData) {
+  Last(allData);
 }
 
 var id1 = setInterval(() => {
@@ -46,7 +65,8 @@ var id1 = setInterval(() => {
     Engine !== undefined &&
     Engine.ev !== "" &&
     Engine.hero.d.nick !== undefined &&
-    asdasd.map !== null
+    asdasd.map !== null &&
+    Engine.battle !== undefined
   ) {
     clearInterval(id1);
     start();
@@ -55,5 +75,120 @@ var id1 = setInterval(() => {
 
 function start() {
   console.log("Loot Load");
-  API.addCallbackToEvent("close_battle", zapiszloot);
+  var tmp = Engine.battle.updateData;
+  Engine.battle.updateData = (data, allData) => {
+    var ret = tmp(data, allData);
+    allData.f.m.forEach((txt) => {
+      mlog.push(txt);
+    });
+    if (
+      allData.hasOwnProperty("dead") &&
+      allData.hasOwnProperty("matchmaking_state")
+    ) {
+      //Dead
+      Dead(allData);
+    } else if (
+      allData.f.hasOwnProperty("init") &&
+      allData.f.hasOwnProperty("endBattle") &&
+      allData.f.hasOwnProperty("auto") &&
+      allData.hasOwnProperty("dead") == false
+    ) {
+      //Oneclick kill
+      oneClick(allData);
+    } else if (allData.f.hasOwnProperty("init")) {
+      //Pierwsze
+      First(allData);
+    } else if (
+      allData.f.hasOwnProperty("endBattle") &&
+      allData.f.hasOwnProperty("m")
+    ) {
+      //Last
+      Last(allData);
+    }
+
+    if (allData.hasOwnProperty("loot") && send.team.length !== 1) {
+      //Loot with team
+      lootchat();
+    }
+    return ret;
+  };
+}
+
+function lootchat() {
+  var tmploot = Engine.chat.setPrivInGeneral;
+  Engine.chat.setPrivInGeneral = (t, e) => {
+    var retloot = tmploot(t, e);
+    var msg = [];
+    if (e.t.includes("ITEM#") == true) {
+      if (e.t.includes("Podział łupów") == true) {
+        var textmsg = e.t
+          .replace("[b]Podział łupów[/b]:", "")
+          .replace(" otrzymał", "")
+          .replaceAll('"', "")
+          .replaceAll("(", "")
+          .replaceAll(")", "")
+          .split(":");
+        textmsg.forEach((txt) => {
+          if (txt.includes("ITEM#") == true) {
+            txt.split("ITEM#").forEach((ele) => {
+              msg.push(ele);
+            });
+          } else {
+            msg.push(txt);
+          }
+        });
+        console.log(msg);
+        updatevsMob(msg);
+      }
+    }
+    return retloot;
+  };
+}
+
+function sendvsMob(send) {
+  var data = JSON.stringify(send);
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:8080/addvsMob",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    dataType: "json",
+    success: function (res) {
+      console.log(res);
+    },
+  });
+}
+
+function sendvsPlayer(send) {
+  var data = JSON.stringify(send);
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:8080/addvsPlayer",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    dataType: "json",
+    success: function (res) {
+      console.log(res);
+    },
+  });
+}
+
+function updatevsMob(data) {
+  var data = JSON.stringify({ ev: send.ev, kto: data });
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:8080/updatevsMob",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    dataType: "json",
+    success: function (res) {
+      console.log(res);
+    },
+  });
 }
